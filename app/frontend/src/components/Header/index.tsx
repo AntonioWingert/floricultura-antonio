9900c738c7c9c062e1233c22aa2aeae2afd86e5d
antoniowingert <tonho-bruno@outlook.com>
2023-06-26 16:08:56 -0300

    updates

--- app/frontend/src/components/Header/index.tsx
+++ app/frontend/src/components/Header/index.tsx
@@ -6,8 +6,8 @@ import CustomButton from '../CustomButton';
 export default function Header() {
   return (
     <S.Container>
-      <CustomTitle $size="md" $as="h2" $color="danger">
-        olá mundo legallllllllll
+      <CustomTitle $size="md" $as="h1" $color="primary">
+        olá mundo
       </CustomTitle>
       <CustomButton $variable="primary">button</CustomButton>
     </S.Container>
